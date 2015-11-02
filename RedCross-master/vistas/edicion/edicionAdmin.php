76f881118d07fbc474e98632067d8aa4aa743abc
Ariana Cisneros Garza <ariana.cisneros@live.com>
2015-11-01 23:04:05 -0600

    edicion de admnistrador

--- RedCross-master/vistas/edicion/edicionAdmin.php
+++ RedCross-master/vistas/edicion/edicionAdmin.php
@@ -9,11 +9,11 @@ include "../../includes/sessionAdmin.php";
 	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
-	
+
 	<title>Edici&oacute;n</title>
 
 	<link rel="shortcut icon" href="assets/images/gt_favicon.png">
-	
+
 	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
@@ -21,16 +21,64 @@ include "../../includes/sessionAdmin.php";
 	<!-- Custom styles for our template -->
 	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen" >
 	<link rel="stylesheet" href="assets/css/main.css">
+	<script src="../../includes/javascript_util.js"></script>
 
 	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
 	<script src="assets/js/html5shiv.js"></script>
 	<script src="assets/js/respond.min.js"></script>
 	<![endif]-->
-</head>
+
+	<script>
+	function selectOption(select, textOption){
+	for(option in select.options){
+		if(textOption == option.text){
+			option.selected = true;
+			break;
+		}
+	}
+	}
+		function search(){
+		var searchId = document.getElementById('searchId').value;
+		console.log(searchId);
+		if(!isValidMatricula(searchId)){
+			alert("Favor de ingresar una matricula valida");
+			return;
+		}
+			xhr=new XMLHttpRequest();
+			xhr.onload= fillFields;
+			var url="../../controladores/edicion/search.php?matricula=" + searchId;
+			xhr.open("GET", url, true);
+			xhr.send();
+		}
+		function fillFields(){
+			//alert(xhr.responseText);
+			//console.log(xhr.responseText);
+			var fields = xhr.responseText.trim();
+			var arrayFields = fields.split("|");
+			if(arrayFields[0] == "-1"){
+				alert(arrayFields[1]);
+				return;
+			}
+
+
+			document.getElementById('matricula').value = arrayFields[1];
+			document.getElementById('nombres').value = arrayFields[3];
+			document.getElementById('APaterno').value = arrayFields[4];
+			document.getElementById('AMaterno').value = arrayFields[5];
+			document.getElementById('FechaNacimiento').value = arrayFields[6];
+			document.getElementById('CURP').value = arrayFields[14];
+			document.getElementById('Enfermedades').value = arrayFields[17];
+			document.getElementById('Alergias').value = arrayFields[18];
+			document.getElementById('Telefono').value = arrayFields[26];
+			document.getElementById('Email').value = arrayFields[30];
+
+		}
+	</script>
+	</head>
 
 <body class="home">
-	
+
 	<nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<div class="navbar-header">
@@ -42,13 +90,12 @@ include "../../includes/sessionAdmin.php";
 				</button>
 				<a class="navbar-brand" href="#">Edici&oacute;n</a>
 			</div>
-			<form class="navbar-form navbar-left" role="search">
+			<form class="navbar-form navbar-left" role="search" onSubmit="return false;">
 				<div class="form-group">
-					<input type="text" class="form-control" placeholder="Ingresar ID">
+					<input type="text" id="searchId" class="form-control" placeholder="Ingresar ID">
 				</div>
-				<button type="submit" class="btn btn-default">Buscar</button>
+				<button onclick="search()" class="btn btn-default">Buscar</button>
 			</form>
-
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right">
 					<li><a href="menuAdmin.html">Regresar</a></li>
@@ -59,13 +106,14 @@ include "../../includes/sessionAdmin.php";
 
 	<!-- Intro -->
 	<div class="container text-center">
-		<h2 class="thin">Edici&oacute;n de datos de Maestro</h2>
+		<h2 class="thin">Edici&oacute;n de datos de Administrador</h2>
 		<br>
-
 	</div>
 	<!-- /Intro-->
 
 	<!-- Highlights - jumbotron -->
+	<form method="post" action="../../controladores/edicion/administrador.php">
+		<input type="hidden"  id="matricula" name="matricula" value="">
 	<div class="">
 		<div class="container">
 			<div class="row">
@@ -73,79 +121,66 @@ include "../../includes/sessionAdmin.php";
 				<div class="form-group">
 					<label for="" class="col-lg-2 control-label">Nombre(s)</label>
 					<div class="col-lg-10">
-						<input type="text" class="form-control" id="" name="" placeholder="Nombre" >
+						<input type="text" class="form-control" id="nombres" name="nombres" placeholder="Nombre" >
 					</div>
 				</div>
 				<br><br>
 				<div class="form-group">
 					<label for="" class="col-lg-2 control-label">Apellido Paterno</label>
 					<div class="col-lg-10">
-						<input type="text" class="form-control" id="" name="" placeholder="Apellido Paterno" >
+							<input type="text" class="form-control" id="APaterno" name="APaterno" placeholder="Apellido Paterno" >
 					</div>
 				</div>
 				<br><br>
 				<div class="form-group">
 					<label for="" class="col-lg-2 control-label">Apellido Materno</label>
 					<div class="col-lg-10">
-						<input type="text" class="form-control" id="" name="" placeholder="Apellido Materno" >
+						<input type="text" class="form-control" id="AMaterno" name="AMaterno" placeholder="Apellido Materno" >
 					</div>
 				</div>
 				<br><br>
 				<div class="form-group">
 					<label for="" class="col-lg-2 control-label">Fecha Nacimiento</label>
 					<div class="col-lg-10">
-						<input type="date" class="form-control" id="" name="" placeholder="dd/mm/aaaa" >
+						<input type="date" class="form-control" id="FechaNacimiento" name="FechaNacimiento" placeholder="dd/mm/aaaa" >
 					</div>
 				</div>
 				<br><br>
 				<div class="form-group">
 					<label for="" class="col-lg-2 control-label">CURP</label>
 					<div class="col-lg-10">
-						<input type="text" class="form-control" id="" name="" placeholder="CURP" >
+						<input type="text" class="form-control" id="CURP" name="CURP" placeholder="CURP" >
 					</div>
 				</div>
 				<br><br>
 				<div class="form-group">
 					<label for="" class="col-lg-2 control-label">Enfermedades</label>
 					<div class="col-lg-10">
-						<input type="text" class="form-control" id="" name="" placeholder="Liste enfermedades que padece" >
+						<input type="text" class="form-control" id="Enfermedades" name="Enfermedades" placeholder="Liste enfermedades que padece" >
 					</div>
 				</div>
 				<br><br>
 				<div class="form-group">
 					<label for="" class="col-lg-2 control-label">Alergias</label>
 					<div class="col-lg-10">
-						<input type="text" class="form-control" id="" name="" placeholder="Liste alergias que padece" >
+						<input type="text" class="form-control" id="Alergias" name="Alergias" placeholder="Liste alergias que padece" >
 					</div>
 				</div>
 				<br><br>
 				<div class="form-group">
 					<label for="" class="col-lg-2 control-label">Tel&eacute;fono local o celular</label>
 					<div class="col-lg-10">
-						<input type="text" class="form-control" id="" name="" placeholder="Tel&eacute;fono" >
+						<input type="text" class="form-control" id="Telefono" name="Telefono" placeholder="Tel&eacute;fono" >
 					</div>
 				</div>
 				<br><br>
 				<div class="form-group">
 					<label for="" class="col-lg-2 control-label">Email</label>
 					<div class="col-lg-10">
-						<input type="email" class="form-control" id="" name="" placeholder="Email" >
+						<input type="email" class="form-control" id="Email" name="Email" placeholder="Email" >
 					</div>
 				</div>
 				<br><br>
-				<div class="form-group">
-					<label for="" class="col-lg-2 control-label">Contraseña</label>
-					<div class="col-lg-10">
-						<input type="password" class="form-control" id="" name="" placeholder="Contraseña" >
-					</div>
-				</div>
-				<br><br>
-				<div class="form-group">
-					<label for="" class="col-lg-2 control-label">Repite contraseña</label>
-					<div class="col-lg-10">
-						<input type="password" class="form-control" id="" name="" placeholder="Contraseña" >
-					</div>
-				</div>
 			</div> <!-- /row  -->
 			<br><br>
 			<div class="col-lg-12 text-right">
@@ -172,7 +207,7 @@ include "../../includes/sessionAdmin.php";
 							<p> Avenida Alfonso Reyes Norte #2503 Norte, Del Prado, 64410 Monterrey, N.L. <br>
 								<a href="mailto:#">cruz.roja@cr.com</a><br>
 								81-1477-1477
-							</p>	
+							</p>
 						</div>
 					</div>
 
@@ -182,7 +217,7 @@ include "../../includes/sessionAdmin.php";
 							<p class="follow-me-icons clearfix">
 								<a href=""><i class="fa fa-twitter fa-2"></i></a>
 								<a href=""><i class="fa fa-facebook fa-2"></i></a>
-							</p>	
+							</p>
 						</div>
 					</div>
 
@@ -204,7 +239,7 @@ include "../../includes/sessionAdmin.php";
 					<div class="col-md-6 widget">
 						<div class="widget-body">
 							<p class="simplenav">
-								<a href="#">Home</a> | 
+								<a href="#">Home</a> |
 								<a href="#">Contacto</a> |
 								<b><a href="#">Iniciar sesi&oacute;n</a></b>
 							</p>
@@ -214,7 +249,7 @@ include "../../includes/sessionAdmin.php";
 					<div class="col-md-6 widget">
 						<div class="widget-body">
 							<p class="text-right">
-								Copyright &copy; 2015, Cruz Roja. 
+								Copyright &copy; 2015, Cruz Roja.
 							</p>
 						</div>
 					</div>
@@ -222,7 +257,7 @@ include "../../includes/sessionAdmin.php";
 				</div> <!-- /row of widgets -->
 			</div>
 		</div>
-	</footer>	
+	</footer>
 
 	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
